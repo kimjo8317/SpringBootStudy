4e0ec422af693eef03d992ef91f00860b8c72b87
kimjunoh <kimjo8317@gmail.com>
2023-04-28 00:23:41 +0900

    2

--- src/main/java/com/mysite/sbb/HelloLombok.java
+++ src/main/java/com/mysite/sbb/HelloLombok.java
@@ -2,7 +2,7 @@ package com.mysite.sbb;
 
 import lombok.Getter;
 
-@Getter
+@Getter1
 //@Setter
 public class HelloLombok {
 
--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -2,7 +2,7 @@ package com.mysite.sbb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuestionRepository extends JpaRepository <Question, Integer>
+public interface QuestionRepository extends JpaRepository <Question, Integer>//QuestionRepository를 리포지토리로만듬
 {
 
 }
